e080cb3a91f28862a9215b0ced4dd5588f81833a
abhishek <abhishekbahukhandi2020@gmail.com>
2024-07-10 12:59:56 +0530

    updated

--- src/Components/AboutContent.js
+++ src/Components/AboutContent.js
@@ -14,10 +14,16 @@ const AboutContent = () => {
             ></video>
         </div>
         <h1>Who Am I?</h1>
-        <p>As a final year student at Maharaja Agrasen Institute of Technology, I have discovered a strong passion for software development. With a focus on designing and coding, I have developed a range of skills and experience in this field. As a fulllstack developer from India, I am committed to creating engaging and visually appealing websites for clients. I am skilled in tech like <u>Html</u>, <u>Css</u>, <u>javascript</u>, <u>React js</u>, <u>Tailwind</u>, <u>Bootstrap</u>, and <u>Firebase</u>, currently aspiring to learn machine learning and backend technologies.  </p>
+<p>
+    As a final year student at <strong>Maharaja Agrasen Institute of Technology</strong>, I have discovered a strong passion for <strong>software development</strong>. With a focus on <strong>designing</strong> and <strong>coding</strong>, I have developed a range of skills and experience in this field. As a <strong>fullstack developer</strong> from India, I am committed to creating engaging and visually appealing websites for clients. I am skilled in tech like <strong>Html</strong>, <strong>Css</strong>, <strong>JavaScript</strong>, <strong>React js</strong>, <strong>Tailwind</strong>, <strong>Bootstrap</strong>, and <strong>Firebase</strong>. Additionally, I possess strong backend skills with <strong>Node.js</strong>, <strong>Express.js</strong>, <strong>MongoDB</strong>, and <strong>SQL</strong>. I am currently aspiring to learn cloud technologies like <strong>AWS</strong> and <strong>Kubernetes</strong>.
+</p>
+
         <br />
         <h1>Previous internship</h1>
-        <p>During my internship at Suvidha Foundation as a front-end developer, I was responsible for designing and coding their website while also learning about <u>Git and GitHub</u>. This experience provided me with valuable technical skills and knowledge in the field of front-end development. As a highly motivated individual, I am now seeking new opportunities to further expand my skills and contribute to the growth of an organization. I am passionate about creating visually appealing and user-friendly websites, and am confident in my ability to bring value to any team.</p>
+        <p>
+    Led the development of <a href="https://safeyourweb.com/" target="_blank"><strong>safeyourweb.com</strong></a> and <a href="https://prosecdevs.com/" target="_blank"><strong>prosecdevs.com</strong></a>, implementing <strong>role-based backend access</strong> and creating admin panels using <strong>Node.js</strong> and <strong>Express.js</strong>. Crafted visually appealing UIs for both sites with <strong>React-TSX</strong>, demonstrating attention to detail and a user-centric design approach. Additionally, executed various small projects, showcasing adaptability and continuous learning in diverse technology landscapes, including converting the backend API of iBharat (an Indian search engine) from <strong>PHP</strong> to <strong>Node.js</strong> using <strong>ChatGPT</strong>.
+</p>
+
         <Link to="/contact" className="btn">Contact</Link>
     </div>
     )
